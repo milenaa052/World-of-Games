e1a7f52e0717c8193f0693f6c59ce913399f61d1
milenaa_052 <milenasantosdeoliveira40@gmail.com>
2024-05-27 20:47:12 -0300

    adicionando cards para os jogos

--- paginas/home.php
+++ paginas/home.php
@@ -44,4 +44,58 @@
         <!--<div class="swiper-button-prev"></div>
         <div class="swiper-button-next"></div>-->
     </div>
+</section>]
+
+<section class="infoCards animate__animated animate__fadeInUp">
+    <h1 class="text-center">Lançamentos</h1>
+
+    <div class="cards d-flex justify-content-center gap-4">
+        <div class="card mb-3" style="max-width: 540px;">
+            <div class="row g-0">
+                <div class="col-md-4">
+                    <img src="img/subway-surfers.webp" class="img-fluid rounded-start" alt="...">
+                </div>
+                <div class="col-md-8 card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    <a href="#" class="btn glow-on-hover pulse">Jogue Agora</a>
+                </div>
+            </div>
+        </div>
+
+        <div class="card mb-3" style="max-width: 540px;">
+            <div class="row g-0">
+                <div class="col-md-4">
+                    <img src="img/subway-surfers.webp" class="img-fluid rounded-start" alt="...">
+                </div>
+                <div class="col-md-8 card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    <a href="#" class="btn glow-on-hover pulse">Jogue Agora</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="ultimosCards">
+    <h1 class="text-center">Você também pode gostar de:</h1>
+
+    <div class="cards d-flex justify-content-center gap-5">
+        <div class="card" style="width: 18rem;">
+            <img src="img/gta.jpg" class="card-img-top" alt="...">
+            <div class="card-body d-flex justify-content-between">
+            <h5 class="card-title align-content-center">GTA V</h5>
+            <a href="#" class="btn glow-on-hover pulse">Jogue Agora</a>
+            </div>
+        </div>
+
+        <div class="card" style="width: 18rem;">
+            <img src="img/race-car.jpg" class="card-img-top" alt="...">
+            <div class="card-body d-flex justify-content-between">
+            <h5 class="card-title align-content-center">Call of Duty</h5>
+            <a href="#" class="btn glow-on-hover pulse">Jogue Agora</a>
+            </div>
+        </div>
+    </div>
 </section>
