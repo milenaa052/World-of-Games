c9928387d04cb6d107cf9eadc8af05b25226a483
milenaa_052 <milenasantosdeoliveira40@gmail.com>
2024-06-07 17:12:05 -0300

    tela de cadastro

---
+++ paginas/cadastro.php
@@ -0,0 +1,26 @@
+<section class="cadastro">
+    <h1 class="text-center" data-aos="fade-up" data-aos-duration="800" data-aos-easing="ease-in-sine">Cadastro</h1>
+
+    <form action="" class="card d-flex flex-column justify-content-center align-items-center" data-aos="fade-up" data-aos-duration="800" data-aos-easing="ease-in-sine">
+        <div class="campos mb-3 col-12 col-sm-6">
+            <label for="nome" class="form-label">Nome</label>
+            <input type="text" class="form-control fw-bold" id="nome" placeholder="Digite o seu nome" required>
+        </div>
+
+        <div class="campos mb-3 col-12 col-sm-6">
+            <label for="email" class="form-label">Email</label>
+            <input type="email" class="form-control fw-bold" id="email" placeholder="email@gmail.com" required>
+        </div>
+
+        <div class="campos mb-3 col-12 col-sm-6">
+            <label for="senha" class="form-label">Senha</label>
+            <input type="password" class="form-control fw-bold" id="senha" placeholder="Digite a sua senha" required>
+        </div>
+
+        <div class="col-12 col-md-6 d-flex justify-content-end gap-2">
+            <span class="d-flex align-items-center">Já possui conta? Faça login</span>
+            <button type="submit" class="btn">Login</button>
+            <button type="submit" class="btn">Cadastrar</button>
+        </div>
+    </form>
+</section>
