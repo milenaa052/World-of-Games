77d425aa209b14bdb9a9cea6ebbc2ff4a36e34c9
milenaa_052 <milenasantosdeoliveira40@gmail.com>
2024-06-16 17:11:05 -0300

    Corrigindo conexão com banco de dados

--- config.php
+++ config.php
@@ -1,5 +1,16 @@
 <?php
 
-    $conn = mysqli_connect('monorail.proxy.rlwy.net','root','PVrlYjcwddbqsFgjYZSFVeZfRXgOiApi','railway') or die('connection failed');
+$dbHost = 'monorail.proxy.rlwy.net:52749';
+$dbUsername = 'root';
+$dbPassword = 'PVrlYjcwddbqsFgjYZSFVeZfRXgOiApi';
+$dbName = 'railway';
+
+$conn = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName);
+
+// if ($conn->connect_error) {
+//     die("Connection failed: " . $conn->connect_error);
+// } else {
+//     echo "Connection made successfully";
+// }
 
 ?>
