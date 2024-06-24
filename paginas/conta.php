b90a6ec606a92dda996de539ac8c062df41ff477
milenaa_052 <milenasantosdeoliveira40@gmail.com>
2024-06-24 14:51:55 -0300

    ajustes erro

--- paginas/conta.php
+++ paginas/conta.php
@@ -10,11 +10,53 @@
         echo '</script>';
         exit;
     };
+
+    if(isset($_POST['update_profile'])){
+
+        $update_name = mysqli_real_escape_string($conn, $_POST['update_name']);
+        $update_email = mysqli_real_escape_string($conn, $_POST['update_email']);
+     
+        mysqli_query($conn, "UPDATE `user_form` SET name = '$update_name', email = '$update_email' WHERE id = '$user_id'") or die('query failed');
+
+        $old_pass = mysqli_real_escape_string($conn, $_POST['old_pass']);
+        $update_pass = mysqli_real_escape_string($conn, $_POST['update_pass']);
+        $new_pass = mysqli_real_escape_string($conn, $_POST['new_pass']);
+     
+        if(!empty($update_pass) || !empty($new_pass)){
+            if($update_pass === $old_pass){
+                if ($update_pass === $new_pass) {
+                    $message[] = 'A nova senha deve ser diferente da senha atual';
+                } else {
+                    mysqli_query($conn, "UPDATE `user_form` SET password = '$new_pass' WHERE id = '$user_id'") or die('query failed');
+                    $sucessMessage[] = 'Senha atualizada com sucesso!';
+                }
+            }else {
+                $message[] = 'Senha antiga não corresponde!';
+            }
+        }
+     
+        $update_image = $_FILES['update_image']['name'];
+        $update_image_size = $_FILES['update_image']['size'];
+        $update_image_tmp_name = $_FILES['update_image']['tmp_name'];
+        $update_image_folder = './img/uploaded_img/'.$update_image;
+     
+        if(!empty($update_image)){
+           if($update_image_size > 2000000){
+              $message[] = 'O tamanho da imagem é muito grande!';
+           }else{
+              $image_update_query = mysqli_query($conn, "UPDATE `user_form` SET image = '$update_image' WHERE id = '$user_id'") or die('query failed');
+              if($image_update_query){
+                 move_uploaded_file($update_image_tmp_name, $update_image_folder);
+              }
+              $sucessMessage[] = 'Imagem atualizada com sucesso!';
+           }
+        }
+    }
 ?>
 
 <section class="conta">
     <div class="w-100 d-flex flex-column justify-content-center">
-        <form action="index.php?pg=updateConta" method="POST" enctype="multipart/form-data" class="perfil d-flex flex-column justify-content-center align-items-center">
+        <form action="" method="POST" enctype="multipart/form-data" class="perfil d-flex flex-column justify-content-center align-items-center">
         <div class="image-upload">
                 <?php
                     $select = mysqli_query($conn, "SELECT * FROM `user_form` WHERE id = '$user_id'") or die('query failed');
